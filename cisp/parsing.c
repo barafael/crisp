5d9674a47cfa095d8f302696e64f8bcf16d17d94
Rafael Bachmann <bachmann@fim.uni-passau.de>
2017-02-08 21:55:39 +0100

    add mpc library

---
+++ cisp/parsing.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <histedit.h>
+
+#include "mpc.h"
+
+/* Declare input buffer */
+static char input[2048];
+
+int main(int argc, char** argv) {
+    /* version and exit information */
+    puts("Lispy version 0.0.0.0.1\n");
+    puts("Press CTRL-C to exit\n");
+
+    while (1) {
+        char* input = readline("cispy >> ");
+
+        add_history(input);
+
+        printf("%s\n", input);
+
+        /* readline does malloc */
+        free(input);
+    }
+    return 0;
+}
+
