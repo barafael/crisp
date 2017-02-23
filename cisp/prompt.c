c0b2b2bc9eec28baa09706e2b718c0121938e804
Rafael Bachmann <bachmann@fim.uni-passau.de>
2017-02-23 14:51:27 +0100

    formatter

--- cisp/prompt.c
+++ cisp/prompt.c
@@ -8,20 +8,20 @@
 static char input[2048];
 
 int main(int argc, char** argv) {
-    /* version and exit information */
-    puts("Lispy version 0.0.0.0.1\n");
-    puts("Press CTRL-C to exit\n");
+	/* version and exit information */
+	puts("Lispy version 0.0.0.0.1\n");
+	puts("Press CTRL-C to exit\n");
 
-    while (1) {
-        char* input = readline("cispy >> ");
+	while (1) {
+		char* input = readline("cispy >> ");
 
-        add_history(input);
+		add_history(input);
 
-        printf("%s\n", input);
+		printf("%s\n", input);
 
-        /* readline does malloc */
-        free(input);
-    }
-    return 0;
+		/* readline does malloc */
+		free(input);
+	}
+	return 0;
 }
 
